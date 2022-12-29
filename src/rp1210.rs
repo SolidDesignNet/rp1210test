3faedecc5bcec4e09473c2321b1c19f43dcb0eca
Joe Batt <Joe@SolidDesign.net>
2022-12-28 21:29:37 -0500

    rp1210 drivers are not thread safe

--- src/rp1210.rs
+++ src/rp1210.rs
@@ -11,20 +11,6 @@ use libloading::os::windows::Symbol as WinSymbol;
 
 pub const PACKET_SIZE: usize = 1600;
 
-#[allow(dead_code)]
-fn log<F, T>(msg: &str, mut f: F) -> T
-where
-    F: FnMut() -> T,
-{
-    let start = std::time::Instant::now();
-    println!("{} start", msg);
-    let rtn = f();
-    println!("{} done {:?}", msg, start.elapsed());
-    rtn
-}
-
-// TODO: break out library calls into private struct
-
 type ClientConnectType = unsafe extern "stdcall" fn(i32, i16, *const char, i32, i32, i16) -> i16;
 type SendType = unsafe extern "stdcall" fn(i16, *const u8, i16, i16, i16) -> i16;
 type ReadType = unsafe extern "stdcall" fn(i16, *const u8, i16, i16) -> i16;
@@ -33,11 +19,14 @@ type _VERSION = unsafe extern "stdcall" fn(i16, *const u8, i16, i16) -> i16;
 type GetErrorType = unsafe extern "stdcall" fn(i16, *const u8) -> i16;
 
 pub struct Rp1210 {
-    _lib: Library,
     bus: MultiQueue<J1939Packet>,
     running: Arc<AtomicBool>,
+    api: Arc<Mutex<API>>,
+}
+struct API {
     id: i16,
 
+    _lib: Library,
     client_connect_fn: WinSymbol<ClientConnectType>,
     send_fn: WinSymbol<SendType>,
     read_fn: WinSymbol<ReadType>,
@@ -45,9 +34,8 @@ pub struct Rp1210 {
     get_error_fn: WinSymbol<GetErrorType>,
 }
 
-#[allow(dead_code)]
-impl Rp1210 {
-    pub fn new(id: &str, bus: MultiQueue<J1939Packet>) -> Result<Rp1210> {
+impl API {
+    fn new(id: &str) -> Result<API> {
         Ok(unsafe {
             let lib = Library::new(id.to_string())?;
             let client_connect: Symbol<ClientConnectType> =
@@ -56,10 +44,8 @@ impl Rp1210 {
             let send_command: Symbol<CommandType> = (&lib).get(b"RP1210_SendCommand\0").unwrap();
             let read: Symbol<ReadType> = (&lib).get(b"RP1210_ReadMessage\0").unwrap();
             let get_error: Symbol<GetErrorType> = (&lib).get(b"RP1210_GetErrorMsg\0").unwrap();
-            Rp1210 {
+            API {
                 id: 0,
-                running: Arc::new(AtomicBool::new(false)),
-                bus: bus.clone(),
                 client_connect_fn: client_connect.into_raw(),
                 send_fn: send.into_raw(),
                 read_fn: read.into_raw(),
@@ -68,40 +54,6 @@ impl Rp1210 {
                 _lib: lib,
             }
         })
-    }
-    // load DLL, make connection and background thread to read all packets into queue
-    pub fn run(&mut self, dev: i16, connection: &str, address: u8) -> Result<Box<dyn Fn() -> ()>> {
-        self.running.store(true, Relaxed);
-        let stopper = self.running.clone();
-        let id = self.client_connect(dev, connection, address).unwrap();
-        let read = *self.read_fn;
-        let error_fn = *self.get_error_fn;
-        let mut bus = self.bus.clone();
-
-        let running = self.running.clone();
-        std::thread::spawn(move || {
-            let mut buf: [u8; PACKET_SIZE] = [0; PACKET_SIZE];
-            while running.load(Relaxed) {
-                let size = unsafe { read(id, buf.as_mut_ptr(), PACKET_SIZE as i16, 1) };
-                if size >= 0 {
-                    bus.push(J1939Packet::new_rp1210(&buf[0..size as usize]))
-                } else {
-                    if size < 0 {
-                        // read error
-                        let code = -size;
-                        let size = unsafe { error_fn(code, buf.as_mut_ptr()) } as usize;
-                        let msg = String::from_utf8_lossy(&buf[0..size]).to_string();
-                        println!("RP1210 error: {}: {}", code, msg);
-                    }
-                    std::thread::yield_now();
-                }
-            }
-        });
-        Ok(Box::new(move || stopper.store(false, Relaxed)))
-    }
-    pub fn stop(&self) -> Result<()> {
-        self.running.store(false, Relaxed);
-        Ok(())
     }
     fn send_command(&self, cmd: u16, buf: Vec<u8>) -> Result<i16> {
         self.verify_return(unsafe {
@@ -120,12 +72,7 @@ impl Rp1210 {
             Ok(v)
         }
     }
-    pub fn client_connect(
-        &mut self,
-        dev_id: i16,
-        connection_string: &str,
-        address: u8,
-    ) -> Result<i16> {
+    fn client_connect(&mut self, dev_id: i16, connection_string: &str, address: u8) -> Result<i16> {
         let c_to_print = CString::new(connection_string).expect("CString::new failed");
         let app_packetize = true;
         let id = unsafe {
@@ -155,8 +102,82 @@ impl Rp1210 {
         self.send_command(/*CMD_SET_ALL_FILTERS_STATES_TO_PASS*/ 3, vec![])?;
         Ok(id)
     }
-    pub fn send(&self, packet: &J1939Packet) -> Result<i16> {
+    fn send(&self, packet: &J1939Packet) -> Result<i16> {
         let buf = &packet.packet.data;
         self.verify_return(unsafe { (self.send_fn)(self.id, buf.as_ptr(), buf.len() as i16, 0, 0) })
+    }
+}
+
+#[allow(dead_code)]
+impl Rp1210 {
+    pub fn new(id: &str, bus: MultiQueue<J1939Packet>) -> Result<Rp1210> {
+        Ok(Rp1210 {
+            running: Arc::new(AtomicBool::new(false)),
+            bus: bus.clone(),
+            api: Arc::new(Mutex::new(API::new(id)?)),
+        })
+    }
+    // load DLL, make connection and background thread to read all packets into queue
+    pub fn run(&mut self, dev: i16, connection: &str, address: u8) -> Result<Box<dyn Fn() -> ()>> {
+        self.running.store(true, Relaxed);
+        let stopper = self.running.clone();
+        let id = self.client_connect(dev, connection, address).unwrap();
+        let mut bus = self.bus.clone();
+
+        let running = self.running.clone();
+        let api = self.api.clone();
+        std::thread::spawn(move || {
+            let mut buf: [u8; PACKET_SIZE] = [0; PACKET_SIZE];
+            while running.load(Relaxed) {
+                let size = unsafe {
+                    (*api.lock().expect("Unable to access RP1210 API").read_fn)(
+                        id,
+                        buf.as_mut_ptr(),
+                        PACKET_SIZE as i16,
+                        1,
+                    )
+                };
+                if size >= 0 {
+                    bus.push(J1939Packet::new_rp1210(&buf[0..size as usize]))
+                } else {
+                    if size < 0 {
+                        // read error
+                        let code = -size;
+                        let size = unsafe {
+                            (*api
+                                .lock()
+                                .expect("Unable to access RP1210 API")
+                                .get_error_fn)(code, buf.as_mut_ptr())
+                        } as usize;
+                        let msg = String::from_utf8_lossy(&buf[0..size]).to_string();
+                        println!("RP1210 error: {}: {}", code, msg);
+                    }
+                    std::thread::yield_now();
+                }
+            }
+        });
+        Ok(Box::new(move || stopper.store(false, Relaxed)))
+    }
+    pub fn stop(&self) -> Result<()> {
+        self.running.store(false, Relaxed);
+        Ok(())
+    }
+    pub fn client_connect(
+        &mut self,
+        dev_id: i16,
+        connection_string: &str,
+        address: u8,
+    ) -> Result<i16> {
+        self.api
+            .lock()
+            .expect("Unable to access RP1210 API")
+            .client_connect(dev_id, connection_string, address)
+    }
+
+    pub fn send(&self, packet: &J1939Packet) -> Result<i16> {
+        self.api
+            .lock()
+            .expect("Unable to access RP1210 API")
+            .send(packet)
     }
 }
